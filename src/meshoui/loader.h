3eb8c493d9060c8f7cab060bf6ad1d0fd6189f93
Pierre-Yves Wuillaume <pierre-yves.wuillaume@dice-engineering.com>
2019-09-24 18:05:45 +0200

    Add OpenMesh.

--- src/meshoui/loader.h
+++ src/meshoui/loader.h
@@ -8,11 +8,13 @@
 #ifndef MESHOUI_LOADER_H
 #define MESHOUI_LOADER_H
 
-#include <string>
+//#include <string>
+#include "OpenMesh/Core/IO/MeshIO.hh"
+#include "OpenMesh/Core/Mesh/TriMesh_ArrayKernelT.hh"
 
 namespace meshoui {
 
 
-}  // end namespace meshoui
+} // end namespace meshoui
 
-#endif // MESHOUI_MESHOUI_H
+#endif // MESHOUI_LOADER_H
