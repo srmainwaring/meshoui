4c205be6a7cc5097f28a9407f6d225255e43e474
François Rongère <francois.rongere@ec-nantes.fr>
2020-09-30 00:10:52 +0200

    Finalizing a mehouix command line tool to perform remeshing on .obj mesh files

--- cmake/Add_OpenMesh.cmake
+++ cmake/Add_OpenMesh.cmake
@@ -10,17 +10,12 @@ if (NOT OpenMesh_FOUND)
             )
 
     FetchContent_GetProperties(OpenMesh)
-    #        message(STATUS OpenMesh)
-    #        message(STATUS ${openmesh_POPULATED})
-    #        message(STATUS ${openmesh_SOURCE_DIR})
-    #        message(STATUS ${openmesh_BINARY_DIR})
     if(NOT openmesh_POPULATED)
         message(STATUS "Downloading, Configuring and Generating 'OpenMesh' dependency")
         FetchContent_Populate(OpenMesh)
 
         # OpenMesh BUILD OPTIONS
-        set(BUILD_APPS ON) # TODO: mettre OFF
-
+        set(BUILD_APPS OFF) # TODO: mettre OFF
 
         add_subdirectory(${openmesh_SOURCE_DIR} ${openmesh_BINARY_DIR})
     else()
@@ -28,30 +23,11 @@ if (NOT OpenMesh_FOUND)
     endif()
 endif()
 
-
-
 if (TARGET OpenMeshCore)
     get_target_property(INC OpenMeshCore INTERFACE_INCLUDE_DIRECTORIES)
     message(STATUS "Found OpenMesh : ${OPENMESH_INCLUDE_DIR}")
-
     target_include_directories(OpenMeshCore INTERFACE ${OPENMESH_INCLUDE_DIR})
-
 
 else()
     message(STATUS "OpenMesh target NOT FOUND")
 endif()
-#
-##target_include_directories(ceres INTERFACE ${OpenMesh_SOURCE_DIR}/include)
-##set_target_properties(ceres PROPERTIES INTERFACE_INCLUDE_DIRECTORIES ${OpenMesh_SOURCE_DIR}/include)
-#
-#add_library(ceres_external INTERFACE)
-#target_link_libraries(ceres_external INTERFACE ceres)
-##target_include_directories(ceres_external INTERFACE ${OpenMesh_SOURCE_DIR}/include)
-#
-#get_target_property(CERES_INCLUDE_DIRECTORIES ceres INCLUDE_DIRECTORIES)
-##message(STATUS ${INC})
-#foreach(inc ${CERES_INCLUDE_DIRECTORIES})
-#    target_include_directories(ceres_external INTERFACE ${inc})
-#endforeach()
-
-
--- cmake/URL.conf.cmake
+++ cmake/URL.conf.cmake
@@ -13,3 +13,7 @@ set(OpenMesh_TAG "OpenMesh-8.0")
 # mmg
 set(mmg_URL "https://github.com/MmgTools/mmg.git")
 set(mmg_TAG "v5.4.3")
+
+# cxxopts
+set(cxxopts_URL https://github.com/jarro2783/cxxopts.git)
+set(cxxopts_TAG v2.2.1)
