194bb7dfeafc2685fc53f24d8f386e33ae414b2b
François Rongère <francois.rongere@dice-engineering.com>
2021-03-31 17:00:04 +0200

    Upgrading MathUtils version

--- cmake/URL.conf.cmake
+++ cmake/URL.conf.cmake
@@ -4,7 +4,7 @@ set(googletest_TAG release-1.8.1)
 
 # MathUtils
 set(mathutils_URL git@frydom-ce.org:ce/mathutils.git)
-set(mathutils_TAG v1.3.1)
+set(mathutils_TAG v1.4)
 
 # OpenMesh
 set(OpenMesh_URL "https://www.graphics.rwth-aachen.de:9000/OpenMesh/OpenMesh.git")
