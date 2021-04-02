32096a0811b3c078167ab24da5e38a925096b7d1
Lucas Letournel <lucas.letournel@dice-engineering.com>
2021-04-02 10:10:26 +0200

    [WIP] MathUtils updated to v1.5

--- cmake/URL.conf.cmake
+++ cmake/URL.conf.cmake
@@ -1,19 +1,19 @@
 # GoogleTest
 set(googletest_URL https://github.com/google/googletest.git)
-set(googletest_TAG release-1.8.1)
+set(googletest_TAG release-1.10.0 CACHE STRING "")
 
 # MathUtils
 set(mathutils_URL git@frydom-ce.org:ce/mathutils.git)
-set(mathutils_TAG v1.4)
+set(mathutils_TAG v1.5 CACHE STRING "MathUtils version")
 
 # OpenMesh
 set(OpenMesh_URL "https://www.graphics.rwth-aachen.de:9000/OpenMesh/OpenMesh.git")
-set(OpenMesh_TAG "OpenMesh-8.0")
+set(OpenMesh_TAG "OpenMesh-8.0" CACHE STRING "OpenMesh version")
 
 # mmg
 set(mmg_URL "https://github.com/MmgTools/mmg.git")
-set(mmg_TAG "v5.4.3")
+set(mmg_TAG "v5.4.3" CACHE STRING "MMG version")
 
 # cxxopts
 set(cxxopts_URL https://github.com/jarro2783/cxxopts.git)
-set(cxxopts_TAG v2.2.1)
+set(cxxopts_TAG v2.2.1 CACHE STRING "cxxopts version")
