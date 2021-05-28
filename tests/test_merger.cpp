3c2df5be5bb4675f282e4b969447de08dec1d999
pierre-yves wuillaume <pierre-yves.wuillaume@dice-engineering.com>
2021-05-28 16:56:16 +0200

    Test of the merger.

---
+++ tests/test_merger.cpp
@@ -0,0 +1,47 @@
+// ==========================================================================
+// MeshOui
+//
+// Copyright (c) D-ICE Engineering.
+// All rights reserved.
+// ==========================================================================
+
+#include "meshoui/meshoui.h"
+#include "gtest/gtest.h"
+
+using namespace meshoui;
+
+// Test for checking the mesh merger.
+
+TEST(meshoui_tests, merger) {
+
+  #ifdef USE_VTK
+
+  std::vector<Mesh> meshes;
+  meshes.reserve(3);
+
+  // First mesh.
+  Mesh mesh_1("../../data/Sphere.obj");
+  meshes.push_back(mesh_1);
+//  Show(mesh_1);
+
+  // Second mesh.
+  Mesh mesh_2("../../data/Sphere.obj");
+  mesh_2.Translate(Vector3d(3., 0., 0.)); // Translation for seeing the two meshes.
+  meshes.push_back(mesh_2);
+//  Show(mesh_2);
+
+  // Third mesh.
+  Mesh mesh_3("../../data/Sphere.obj");
+  mesh_3.Translate(Vector3d(0., 3., 0.)); // Translation for seeing the two meshes.
+  meshes.push_back(mesh_3);
+//  Show(mesh_3);
+
+  // Merge.
+  auto mesh_merged = Merger(meshes);
+  Show(mesh_merged);
+
+  Write_VTK(mesh_merged, "Merged_mesh.vtp");
+
+  #endif
+
+}
