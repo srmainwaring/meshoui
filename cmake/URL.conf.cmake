acf0d4bc9659efaa8ae8920674026c18400faabf
Pierre-Yves Wuillaume <pierre-yves.wuillaume@dice-engineering.com>
2021-08-13 11:36:02 +0200

    Update MathUtils.

--- cmake/URL.conf.cmake
+++ cmake/URL.conf.cmake
@@ -4,7 +4,7 @@ set(googletest_TAG release-1.10.0 CACHE STRING "")
 
 # MathUtils
 set(mathutils_URL git@frydom-ce.org:ce/mathutils.git)
-set(mathutils_TAG v1.6.1 CACHE STRING "MathUtils version")
+set(mathutils_TAG v1.7 CACHE STRING "MathUtils version")
 
 # OpenMesh
 set(openmesh_URL "https://www.graphics.rwth-aachen.de:9000/OpenMesh/OpenMesh.git")
